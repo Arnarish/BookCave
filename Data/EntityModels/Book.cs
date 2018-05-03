f62e5648fc25b92f076260902caca78a6691e196
Sindri Dan Garðarsson <sindridan@gmail.com>
2018-05-03 13:19:56 +0000

    eyddi miklu úr index, breytti entitymodels

---
+++ Data/EntityModels/Book.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using System.Web;
+namespace BookCave.Data.EntityModels
+{
+
+    public class Book{
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public bool TopSeller { get; set; }
+        public bool OnSale { get; set; }
+        public int Discount { get; set; }
+        public string Image { get; set; }
+        public double Rating { get; set; }
+
+
+
+    }
+}
