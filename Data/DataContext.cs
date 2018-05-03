a918977d76441c9d31a22ea91e9f00679be7d8c4
Arnar Pálmi <Arnar91@gmail.com>
2018-05-03 12:28:35 +0000

    sql connection added, should be ready to start

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCave.Data
+{
+    public class Datacontext : DbContext
+    {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder
+                    .UseSqlServer(
+                        "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H27;Persist Security Info=False;User ID=VLN2_2018_H27_usr;Password=keenFi$h46;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            }
+    }
+}
