733ebf76a9db86e4cf54f936ff6c7931a7e341d2
Sigthorandri <sigthorandrii@gmail.com>
2018-05-07 12:54:30 +0000

    Validation

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -5,7 +5,9 @@ namespace BookCave.Models.ViewModels
     public class LoginViewModel
     {
         [EmailAddress]
+        [Required(ErrorMessage = "Email is valid")]
         public string Email { get; set; }
+        
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -4,14 +4,15 @@ namespace BookCave.Models.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
         [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Please input a valid password")]
         public string Password { get; set; }
     }
 }
