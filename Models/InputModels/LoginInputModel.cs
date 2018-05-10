5b96990f5bbaae7ca9fb7426eb12a16eb85699e3
Sigthorandri <sigthorandrii@gmail.com>
2018-05-10 16:01:33 +0000

    sma css

--- Models/InputModels/LoginInputModel.cs
+++ Models/InputModels/LoginInputModel.cs
@@ -7,7 +7,6 @@ namespace BookCave.Models.InputModels
         [EmailAddress]
         [Required(ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
-        
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "The password or email was not correct")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
