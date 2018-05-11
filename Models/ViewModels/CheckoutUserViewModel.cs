891966349533ecd1b44995cb4c597ed78e0922fe
hallgrimura17 <hallgrimura17@ru.is>
2018-05-11 23:01:29 +0000

    gagag

--- Models/ViewModels/CheckoutUserViewModel.cs
+++ Models/ViewModels/CheckoutUserViewModel.cs
@@ -9,6 +9,8 @@ namespace BookCave.Models.ViewModels
         public string FullName  { get; set; }
         public string Address    { get; set; }
         public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public string Email      { get; set; }
     }
 }
